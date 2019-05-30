3062fa8d2f6163deec034c2493ac8b8b5a14960b
Alexander K <aleksmetal@yandex.ru>
2019-05-30 23:00:58 +0300

    Добавил функцию в первую задачу

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -12,6 +12,7 @@ function print (text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid (name) {
+return (name ? (name.length <4 || name.indexOf(" ") >= 0 ? false : true) : false)
 }
 
 function sayHello () {
