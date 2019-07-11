11c4a86e7fb8937b377bb3624ab241a5e7594ccf
adkrivoruchko <aleksmetal@yandex.ru>
2019-07-11 21:29:47 +0300

    5 модуль

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,32 +1,35 @@
 'use strict';
 
-/**
- * Компонент, который реализует сортируемую таблицу
- * @param {Array} items - данные, которые нужно отобразить
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      },
- *
- * @constructor
- */
+
 function SortableTable(items) {
 
-    /**
-     * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
-     */
     this.el = document.createElement('table');
 
-    /**
-     * Метод выполняет сортировку таблицы
-     * @param {number} column - номер колонки, по которой нужно выполнить сортировку (отсчет начинается от 0)
-     * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
-     */
-    this.sort = function (column, desc = false) { };
+    let list = items.map(function (i) {
+        return ('<tr><td>' + i.name + '</td><td>' + i.age + '</td><td>' + i.salary + '</td><td>' + i.city + '</td></tr>')
+    }).join('');
+
+    this.el.innerHTML = '<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead><tbody>'+list+'</tbody>'
+    //============================================================================
+    this.sort = function (column, desc = false) {
+    //----------------------------------------------------------------
+        function column_name(item) {
+            if (item === 0) return 'name';
+            if (item === 2) return 'salary';
+        }
+
+        let  col = column_name(column);
+    //--------------------------------------------------------------
+        items.sort(function(a,b){
+            if (a[col] > b[col]) return (desc === false ? 1: -1);
+            if (a[col] < b[col]) return (desc === false ? -1: 1);
+        });
+
+        list = items.map(function(i) {return (
+            '<tr><td>'+i.name+'</td><td>'+i.age+'</td><td>'+i.salary+'</td><td>'+i.city+'</td></tr>'
+        )}).join('');
+
+        this.el.innerHTML = '<thead><tr><td>Name</td><td>Age</td><td>Salary</td><td>City</td></tr></thead><tbody>'+list+'</tbody>'
+    };
 }
 
